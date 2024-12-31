4e5718b2b4fb0cea1c4c09f8e9e25181d0550310
wmayer <wmayer@freecad.org>
2024-12-31 17:19:28 +0100

    [skip ci] Gui: Fix pasting if target object comes from XLink document
    When pasting the copied object then a dialog pops up to save the document beforehand. But due to the wait cursor this is not possible because it blocks
    all user events. So, remove the instantiation of the WaitCursor completely.

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -1182,7 +1182,6 @@ void StdCmdPaste::activated(int iMsg)
         QClipboard* cb = QApplication::clipboard();
         const QMimeData* mimeData = cb->mimeData();
         if (mimeData) {
-            WaitCursor wc;
             getMainWindow()->insertFromMimeData(mimeData);
         }
     }
