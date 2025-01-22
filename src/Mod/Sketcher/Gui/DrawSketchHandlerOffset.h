d1a1937e192aa9bf1fdbcc9bec40e5c487188163
wmayer <wmayer@freecad.org>
2025-01-22 20:38:43 +0100

    Sketcher: Fix const-correctness

--- src/Mod/Sketcher/Gui/DrawSketchHandlerOffset.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerOffset.h
@@ -445,7 +445,7 @@ private:
         commitCommand();
     }
 
-    void jointOffsetCurves(std::vector<int>& listOfOffsetGeoIds)
+    void jointOffsetCurves(const std::vector<int>& listOfOffsetGeoIds)
     {
         if (listOfOffsetGeoIds.empty()) {
             return;
@@ -570,7 +570,7 @@ private:
         }
     }
 
-    void makeOffsetConstraint(std::vector<int>& listOfOffsetGeoIds)
+    void makeOffsetConstraint(const std::vector<int>& listOfOffsetGeoIds)
     {
         SketchObject* Obj = sketchgui->getSketchObject();
 
@@ -890,7 +890,7 @@ private:
         Gui::Command::doCommand(Gui::Command::Doc, stream.str().c_str());
     }
 
-    std::vector<std::vector<int>> generatevCC(std::vector<int>& listOfGeo)
+    std::vector<std::vector<int>> generatevCC(const std::vector<int>& listOfGeo)
     {
         // This function separates all the selected geometries into separate continuous curves.
         SketchObject* Obj = sketchgui->getSketchObject();
@@ -1131,7 +1131,7 @@ private:
         return ppc.firstPos1 != PointPos::none;
     }
 
-    bool isCurveClosed(std::vector<int>& curve)
+    bool isCurveClosed(const std::vector<int>& curve)
     {
         bool closed = false;
         if (curve.size() > 2) {
