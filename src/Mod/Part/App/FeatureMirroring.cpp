581eb1dda08f0b3ee914c6c4980f701d9d3a25c1
wmayer <wmayer@freecad.org>
2025-02-05 17:48:05 +0100

    Part: Adjust Mirroring::execute()
    By fixing TopoShape::makeElementMirror() the method Mirroring::execute() must be adjusted too because
    it doesn't handle the placement of the input shape any more.
    
    There are two ways to do it:
    1. Set Placement property of the Mirror feature to the placement of the input feature.
    2. Apply the placement to transformation matrix that will be applied to the input shape.
    
    Because in all older FreeCAD versions the option 2. was implemented it will be done here, too.
    
    This change is a reimplementation of the commit b3156069a from Dec 13, 2011 to fix the bug
    '0000531: Python API: Mirroring rotated parts produces undesired results' on Mantis

--- src/Mod/Part/App/FeatureMirroring.cpp
+++ src/Mod/Part/App/FeatureMirroring.cpp
@@ -37,6 +37,7 @@
 
 
 #include <Mod/Part/App/PrimitiveFeature.h>
+#include <Mod/Part/App/TopoShapeOpCode.h>
 #include <App/Link.h>
 #include <App/Datums.h>
 
@@ -261,7 +262,18 @@ App::DocumentObjectExecReturn *Mirroring::execute()
         auto shape = Feature::getTopoShape(link, ShapeOption::ResolveLink | ShapeOption::Transform);
         if (shape.isNull())
             Standard_Failure::Raise("Cannot mirror empty shape");
-        this->Shape.setValue(TopoShape(0).makeElementMirror(shape,ax2));
+        gp_Trsf mat;
+        mat.SetMirror(ax2);
+
+        // This is needed to respect the placement of the linked shape because it will be
+        // overridden by this object's placement in Feature::onChanged.
+        // Alternatively, this object's placement can be set to the placement of the linked
+        // shape.
+        TopLoc_Location loc = shape.getShape().Location();
+        gp_Trsf placement = loc.Transformation();
+        mat = placement * mat;
+        BRepBuilderAPI_Transform mkTrf(shape.getShape(), mat);
+        this->Shape.setValue(TopoShape(0).makeElementShape(mkTrf, shape, Part::OpCodes::Mirror));
         copyMaterial(link);
 
         return Part::Feature::execute();
