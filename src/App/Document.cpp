7bc531e060caf2728ac5e732b7c24d75c3ce3faa
bofdahof <172177156+bofdahof@users.noreply.github.com>
2025-02-08 06:26:51 +1000

    unused

--- src/App/Document.cpp
+++ src/App/Document.cpp
@@ -1855,7 +1855,7 @@ private:
                         }
                     }
 
-                    del.deleteFile();
+                    auto [[maybe_unused]] res = del.deleteFile();
                     fn = del.filePath();
                 }
                 else {
@@ -1870,7 +1870,7 @@ private:
                 }
             }
             else {
-                fi.deleteFile();
+                auto [[maybe_unused]] res = fi.deleteFile();
             }
         }
 
@@ -2023,7 +2023,7 @@ private:
             }
             else {
                 try {
-                    fi.deleteFile();
+                    auto [[maybe_unused]] res = fi.deleteFile();
                 }
                 catch (...) {
                     Base::Console().Warning("Cannot remove backup file: %s\n",
@@ -3975,7 +3975,7 @@ Document::importLinks(const std::vector<App::DocumentObject*>& objArray)
     MergeDocuments mimeView(this);
     objs = mimeView.importObjects(str);
     str.close();
-    fi.deleteFile();
+    auto [[maybe_unused]] res = fi.deleteFile();
 
     const auto& nameMap = mimeView.getNameMap();
 
