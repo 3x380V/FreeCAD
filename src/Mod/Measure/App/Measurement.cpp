51f57cd4bd5b2e9a0cbc3acde556177c22761bf3
wmayer <wmayer@freecad.org>
2025-02-09 20:33:31 +0100

    Measure: Fix several crashes in Measurement
    Measurement::getShape() used to call ShapeFinder::getLocatedShape() that failed to return a valid shape in many cases.
    And the calling instance didn't check for a valid shape either which caused a segmentation fault.
    
    Solution:
    Replace ShapeFinder::getLocatedShape() with Part::Feature::getShape() that works more reliably and where needed check
    for a valid shape before accessing it.
    
    This fixes issue 19452 and improves the fix for 19349

--- src/Mod/Measure/App/Measurement.cpp
+++ src/Mod/Measure/App/Measurement.cpp
@@ -286,7 +286,7 @@ MeasureType Measurement::getType()
 
 TopoDS_Shape Measurement::getShape(App::DocumentObject* obj, const char* subName) const
 {
-    return ShapeFinder::getLocatedShape(*obj, subName);
+    return Part::Feature::getShape(obj, subName, true);
 }
 
 
@@ -324,7 +324,7 @@ double Measurement::length() const
 
                 //  Get the length of one edge
                 TopoDS_Shape shape = getShape(*obj, (*subEl).c_str());
-                if (shape.IsNull()) {
+                if (shape.IsNull() || shape.Infinite()) {
                     continue;
                 }
                 const TopoDS_Edge& edge = TopoDS::Edge(shape);
@@ -428,8 +428,6 @@ double Measurement::planePlaneDistance() const
     const auto& objects = References3D.getValues();
     const auto& subElements = References3D.getSubValues();
 
-    std::vector<gp_Pln> planes;
-
     // Get the first plane
     TopoDS_Shape shape1 = getShape(objects[0], subElements[0].c_str());
     const TopoDS_Face& face1 = TopoDS::Face(shape1);
@@ -510,7 +508,7 @@ double Measurement::angle(const Base::Vector3d& /*param*/) const
         if (numRefs == 3) {
             TopoDS_Shape shape0 = getShape(objects.at(0), subElements.at(0).c_str());
             TopoDS_Shape shape1 = getShape(objects.at(1), subElements.at(1).c_str());
-            TopoDS_Shape shape2 = getShape(objects.at(1), subElements.at(2).c_str());
+            TopoDS_Shape shape2 = getShape(objects.at(2), subElements.at(2).c_str());
             if (shape0.ShapeType() != TopAbs_VERTEX || shape1.ShapeType() != TopAbs_VERTEX
                 || shape2.ShapeType() != TopAbs_VERTEX) {
                 throw Base::RuntimeError("Measurement references for 3 point angle are not Vertex");
@@ -675,7 +673,11 @@ double Measurement::volume() const
 
         for (size_t i = 0; i < objects.size(); ++i) {
             GProp_GProps props = GProp_GProps();
-            BRepGProp::VolumeProperties(getShape(objects[i], subElements[i].c_str()), props);
+            TopoDS_Shape shape = getShape(objects[i], subElements[i].c_str());
+            if (shape.IsNull() || shape.Infinite()) {
+                continue;
+            }
+            BRepGProp::VolumeProperties(shape, props);
             result += props.Mass();
         }
     }
@@ -698,7 +700,11 @@ double Measurement::area() const
 
         for (size_t i = 0; i < objects.size(); ++i) {
             GProp_GProps props;
-            BRepGProp::SurfaceProperties(getShape(objects[i], subElements[i].c_str()), props);
+            TopoDS_Shape shape = getShape(objects[i], subElements[i].c_str());
+            if (shape.IsNull() || shape.Infinite()) {
+                continue;
+            }
+            BRepGProp::SurfaceProperties(shape, props);
             result += props.Mass();  // Area is obtained using Mass method for surface properties
         }
     }
@@ -733,7 +739,11 @@ Base::Vector3d Measurement::massCenter() const
                 // Compute inertia properties
 
                 GProp_GProps props = GProp_GProps();
-                BRepGProp::VolumeProperties(getShape((*obj), ""), props);
+                TopoDS_Shape shape = ShapeFinder::getLocatedShape(*(*obj), "");
+                if (shape.IsNull()) {
+                    continue;
+                }
+                BRepGProp::VolumeProperties(shape, props);
                 gprops.Add(props);
                 // Get inertia properties
             }
@@ -809,11 +819,17 @@ bool Measurement::linesAreParallel() const
 
     // Get the first line
     TopoDS_Shape shape1 = getShape(objects[0], subElements[0].c_str());
+    if (shape1.IsNull()) {
+        return false;
+    }
     const TopoDS_Edge& edge1 = TopoDS::Edge(shape1);
     BRepAdaptor_Curve curve1(edge1);
 
     // Get the second line
     TopoDS_Shape shape2 = getShape(objects[1], subElements[1].c_str());
+    if (shape2.IsNull()) {
+        return false;
+    }
     const TopoDS_Edge& edge2 = TopoDS::Edge(shape2);
     BRepAdaptor_Curve curve2(edge2);
 
