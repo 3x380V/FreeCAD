481eee909f6d8271f2b0127053f3aed657166d65
wmayer <wmayer@freecad.org>
2025-02-14 20:14:47 +0100

    Sketch: Fix crash when moving sketch with hidden constraints
    Clicking the eye icon on the constraints panel hides all constraints. This calls SketchObject::setVirtualSpace where the existing Constraint
    objects will be replaced. However, the Constraint objects are also stored inside the member 'solvedSketch' but won't be replaced so that the
    pointers there will become dangling.
    
    If the user starts to drag the sketch the pointers are still dangling and cause a crash when calling 'solvedSketch.moveGeometries'.
    
    The crash can be reproduced with:
    1. Create a sketch
    2. Add a single line
    3. Set a length constraint
    4. Make it a non-driving constraint
    5. Hide it with the eye icon or by unticking the check box
    6. Move the line
    
    The same crash also occurs when calling the methods setLabelDistance() or setLabelPosition() from Python and then trying to move the sketch.
    
    Solution:
    
    To fix the problem add the lightweight method 'Sketch::updateConstraints' and call it inside SketchObject::setVirtualSpace to quickly update
    the pointers after invalidating them.
    
    This fixes issue 19566

--- src/Mod/Sketcher/App/SketchObject.cpp
+++ src/Mod/Sketcher/App/SketchObject.cpp
@@ -1036,6 +1036,7 @@ int SketchObject::setLabelPosition(int ConstrId, float value)
     constNew->LabelPosition = value;
     newVals[ConstrId] = constNew;
     this->Constraints.setValues(std::move(newVals));
+    solvedSketch.updateConstraints({ConstrId}, this->Constraints.getValues());
 
     return 0;
 }
@@ -1070,6 +1071,7 @@ int SketchObject::setLabelDistance(int ConstrId, float value)
     constNew->LabelDistance = value;
     newVals[ConstrId] = constNew;
     this->Constraints.setValues(std::move(newVals));
+    solvedSketch.updateConstraints({ConstrId}, this->Constraints.getValues());
 
     return 0;
 }
@@ -1266,6 +1268,7 @@ int SketchObject::setVirtualSpace(int ConstrId, bool isinvirtualspace)
     newVals[ConstrId] = constNew;
 
     this->Constraints.setValues(std::move(newVals));
+    solvedSketch.updateConstraints({ConstrId}, this->Constraints.getValues());
 
     // Solver didn't actually update, but we need this to inform view provider
     // to redraw
@@ -1301,6 +1304,7 @@ int SketchObject::setVirtualSpace(std::vector<int> constrIds, bool isinvirtualsp
     }
 
     this->Constraints.setValues(std::move(newVals));
+    solvedSketch.updateConstraints(constrIds, this->Constraints.getValues());
 
     // Solver didn't actually update, but we need this to inform view provider
     // to redraw
@@ -1340,6 +1344,7 @@ int SketchObject::toggleVirtualSpace(int ConstrId)
     newVals[ConstrId] = constNew;
 
     this->Constraints.setValues(std::move(newVals));
+    solvedSketch.updateConstraints({ConstrId}, this->Constraints.getValues());
 
     // Solver didn't actually update, but we need this to inform view provider
     // to redraw
