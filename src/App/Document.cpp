43a80ed00768121cda484c9f7e43adb4686d0db2
wmayer <wmayer@freecad.org>
2025-03-11 00:16:37 +0100

    App: Improve handling with freeze status
    * Do not raise an exception when saving a document and an object has 'freeze' status because this breaks
      the project file and may lead to loss of data.
    * Make 'freeze' state persistent
    * When toggling 'freeze' status the only work directly on the selection. Do not use in-list or out-list of
      an object because this makes it quite inflexible and leads to unpredictable behaviour.
    
    This mostly fixes issue 18806

--- src/App/Document.cpp
+++ src/App/Document.cpp
@@ -1315,6 +1315,9 @@ void Document::writeObjects(const std::vector<DocumentObject*>& obj,
                 writer.Stream() << "Error=\"" << Property::encodeAttribute(desc) << "\" ";
             }
         }
+        if ((*it)->isFreezed()) {
+            writer.Stream() << "Freeze=\"1\" ";
+        }
         writer.Stream() << "/>" << '\n';
     }
 
@@ -1510,6 +1513,11 @@ std::vector<DocumentObject*> Document::readObjects(Base::XMLReader& reader)
                                    reader.getAttribute<bool>("Invalid"));
                     if (obj->isError() && reader.hasAttribute("Error")) {
                         d->addRecomputeLog(reader.getAttribute<const char*>("Error"), obj);
+                    }
+                }
+                if (reader.hasAttribute("Freeze")) {
+                    if (reader.getAttribute<long>("Freeze") != 0) {
+                        obj->freeze();
                     }
                 }
             }
