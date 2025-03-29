f13940c68ebdc451cc70284e6a61dd1805cde75b
wmayer <wmayer@freecad.org>
2025-03-29 15:29:44 +0100

    Core: Add SequencerBase::stopStep() and SequencerLauncher::stop()

--- src/Base/Sequencer.h
+++ src/Base/Sequencer.h
@@ -25,7 +25,6 @@
 #define BASE_SEQUENCER_H
 
 #include <cstddef>
-
 #include <FCGlobal.h>
 
 namespace Base
@@ -222,6 +221,11 @@ protected:
      */
     virtual void startStep();
     /**
+     * This method can be reimplemented in sub-classes to do some last action
+     * when a new sequence stops. The default implementation does nothing.
+     */
+    virtual void stopStep();
+    /**
      * This method can be reimplemented in sub-classes to give the user a feedback
      * when the next is performed. The default implementation does nothing. If \a canAbort
      * is true then the pending operation can aborted, otherwise not. Depending on the
@@ -376,6 +380,7 @@ public:
     bool next(bool canAbort = false);
     void setProgress(size_t);
     bool wasCanceled() const;
+    void stop();
 
     SequencerLauncher(const SequencerLauncher&) = delete;
     SequencerLauncher(SequencerLauncher&&) = delete;
