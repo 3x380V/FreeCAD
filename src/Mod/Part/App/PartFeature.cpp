185ce6bd9f4ec8ef36c75ce43384388c28de8cde
wmayer <wmayer@freecad.org>
2025-04-18 18:08:39 +0200

    Part: Use higher tolerance in getCameraAlignmentDirection to check for planar faces
    Fixes the issue in https://forum.freecad.org/viewtopic.php?t=96412

--- src/Mod/Part/App/PartFeature.cpp
+++ src/Mod/Part/App/PartFeature.cpp
@@ -1773,7 +1773,8 @@ bool Feature::getCameraAlignmentDirection(
     }
 
     // Face normal
-    if (topoShape.isPlanar()) {
+    const double planarTol = 0.001;
+    if (topoShape.isPlanar(planarTol)) {
         try {
             const auto face = TopoDS::Face(topoShape.getShape());
             gp_Pnt point;
