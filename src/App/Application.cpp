c468eb32f982865d37331117115bfd594a8bef4b
wmayer <wmayer@freecad.org>
2025-04-26 21:04:07 +0200

    App: Move Python functions from Application to ApplicationPy
    This reduces build time when adding new Python functions because only one file must be compiled.

--- src/App/Application.cpp
+++ src/App/Application.cpp
@@ -104,6 +104,7 @@
 
 #include "Annotation.h"
 #include "Application.h"
+#include "ApplicationPy.h"
 #include "CleanupProcess.h"
 #include "ComplexGeoData.h"
 #include "Services.h"
@@ -263,7 +264,7 @@ void Application::setupPythonTypes()
     Base::PyGILStateLocker lock;
     PyObject* modules = PyImport_GetModuleDict();
 
-    ApplicationMethods = Application::Methods;
+    ApplicationMethods = ApplicationPy::Methods;
     PyObject* pAppModule = PyImport_ImportModule ("FreeCAD");
     if (!pAppModule) {
         PyErr_Clear();
@@ -2506,7 +2507,7 @@ void Application::initConfig(int argc, char ** argv)
 
         auto moduleName = "FreeCAD";
         PyImport_AddModule(moduleName);
-        ApplicationMethods = Application::Methods;
+        ApplicationMethods = ApplicationPy::Methods;
         PyObject *pyModule = init_freecad_module();
         PyDict_SetItemString(sysModules, moduleName, pyModule);
         Py_DECREF(pyModule);
