11efeedc9412781cc2f7299cea8ec29a6aa2c099
wmayer <wmayer@freecad.org>
2025-04-28 16:56:41 +0200

    Part: Implement ShapeAttributeProvider
    Add the interface class CustomAttributeProvider and implement ShapeAttributeProvider to avoid to rely on
    implementatuion details of an extension module in the core system.
    
    This fixes issue 6857

--- src/App/Services.h
+++ src/App/Services.h
@@ -71,6 +71,17 @@ public:
     bool supports(DocumentObject* object) const override;
 };
 
+/**
+* This service should provide custom attribute access of a Python object
+*/
+class CustomAttributeProvider
+{
+public:
+    virtual ~CustomAttributeProvider() = default;
+
+    virtual std::optional<PyObject*> getAttribute(DocumentObject* object, const char* attr) const = 0;
+};
+
 }
 
 
