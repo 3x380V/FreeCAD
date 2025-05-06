0a8fc99a32b52d45231c9172df78f8b85301138b
wmayer <wmayer@freecad.org>
2025-05-06 11:39:48 +0200

    FEM: Fix build failures/warnings

--- src/Mod/Fem/App/PropertyPostDataObject.cpp
+++ src/Mod/Fem/App/PropertyPostDataObject.cpp
@@ -198,6 +198,7 @@ void PropertyPostDataObject::setPyObject(PyObject* value)
     m_dataObject->DeepCopy(dobj);
     hasSetValue();
 #else
+    std::ignore = value;
     throw Base::NotImplementedError();
 #endif
 }
