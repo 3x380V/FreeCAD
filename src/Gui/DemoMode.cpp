79a24ef9140b9a329ea34905afdac61288cb4b86
wmayer <wmayer@freecad.org>
2025-05-23 19:08:09 +0200

    [skip ci] Gui: Suppress linter warnings

--- src/Gui/DemoMode.cpp
+++ src/Gui/DemoMode.cpp
@@ -46,6 +46,7 @@ DemoMode::DemoMode(QWidget* /*parent*/, Qt::WindowFlags fl)
     , viewAxis(0, 0, -1)
     , ui(new Ui_DemoMode)
 {
+    // NOLINTBEGIN
     // create widgets
     ui->setupUi(this);
     setupConnections();
@@ -60,6 +61,7 @@ DemoMode::DemoMode(QWidget* /*parent*/, Qt::WindowFlags fl)
     showHideTimer = new QTimer(this);
     showHideTimer->setInterval(5000);
     connect(showHideTimer, &QTimer::timeout, this, &DemoMode::hide);
+    // NOLINTEND
 }
 
 /** Destroys the object and frees any allocated resources */
